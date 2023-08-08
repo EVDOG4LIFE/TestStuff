915deaf0cb134b46474258d871079421d3148e63
Evan <evanleair@gmail.com>
2023-08-08 14:18:50 -0700

    Update CompareMachine.ps1

--- CompareMachine.ps1
+++ CompareMachine.ps1
@@ -1,10 +1,26 @@
-Import-Module ServerManager
+param (
+    [string]$Server1_Name,
+    [string]$Server2_Name
+)
 
-# Get the roles and features from Server1
-$Server1 = Get-WindowsFeature | Where-Object { $_.Installed -eq $True }
+if (-not $Server1_Name -or -not $Server2_Name) {
+    Write-Host "Please provide both Server1_Name and Server2_Name."
+    return
+}
 
-# Get the roles and features from Server2
-$Server2 = Invoke-Command -ComputerName 'Server2_Name' -ScriptBlock { Get-WindowsFeature | Where-Object { $_.Installed -eq $True } }
+# Function to get the roles and features
+function Get-InstalledFeatures {
+    param (
+        [string]$ServerName
+    )
+    Invoke-Command -ComputerName $ServerName -ScriptBlock {
+        Get-WindowsFeature | Where-Object { $_.Installed -eq $True }
+    }
+}
+
+# Get the roles and features from both servers
+$Server1 = Get-InstalledFeatures -ServerName $Server1_Name
+$Server2 = Get-InstalledFeatures -ServerName $Server2_Name
 
 # Compare the results
 Compare-Object $Server1 $Server2 -Property Name, DisplayName, Installed | Format-Table -AutoSize
